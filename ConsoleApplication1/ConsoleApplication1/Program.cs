d4b59787882c6255658da76540d26e1c384daade
blyat <daniel.bermude@gmail.com>
2016-01-18 21:22:41 -0600

    v1.0
    HelloWorld?...

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            /* my first program in C# */
+            Console.WriteLine("Daniel Bermudez");
+            Console.ReadKey();
+        }
+    }
+}
